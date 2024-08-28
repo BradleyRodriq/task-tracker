3a6ff41121a3e5d66c50aa0d658eb32e0c52dbbb
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-08-28 18:26:54 -0400

    made an app tracker backend

---
+++ backend/models/taskModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  time_spent: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: String,
+    required: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Task', taskSchema)
