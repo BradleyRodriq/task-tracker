bb3c216e2c8f417d303520ac641f1d01f08a3260
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-08-29 19:17:55 -0400

    added backend cors

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/tasks')
 const userRoutes = require('./routes/user')
+const cors = require('cors')
 
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
+
+// Enable CORS for your frontend domain
+app.use(cors({
+  origin: 'https://tasker-p9b3.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
